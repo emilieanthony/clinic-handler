5a7a6ddf8df06dd16846b194187726c91b84f570
Emilie Anthony <emilieanthony@live.se>
2022-01-05 11:33:05 +0100

    change name of main file to ClinicHandler in order to clarify the responsibility of the file

--- Menu.js
+++ Menu.js
@@ -1,26 +1,26 @@
-const inquirer = require('inquirer')
+const inquirer = require("inquirer");
 
-const clinicHandler = require('./main.js')
-
+const clinicHandler = require("./ClinicHandler.js");
 
 /**
  * Waits for user input to close the clinic handler. Closing the clinic handler disconnects the component in a clean way from the system
  */
-module.exports.printMenu = function() {
-    inquirer.prompt( [
-        {
-            type: 'terminate program',
-            message: 'To close the program, press "x" followed by "enter" \n',
-            name: 'closeInput',
-            validate: isX
-        }
+module.exports.printMenu = function () {
+  inquirer
+    .prompt([
+      {
+        type: "terminate program",
+        message: 'To close the program, press "x" followed by "enter" \n',
+        name: "closeInput",
+        validate: isX,
+      },
     ])
-        .then((answer) => {
-            clinicHandler.disconnect()
-            console.log('See you around!')
-            process.exit()
-        })
-}
+    .then((answer) => {
+      clinicHandler.disconnect();
+      console.log("See you around!");
+      process.exit();
+    });
+};
 
 /**
  * Checks if the given value is === to x or X
@@ -28,8 +28,8 @@ module.exports.printMenu = function() {
  * @returns {string|boolean}
  */
 const isX = (value) => {
-    if (value === 'x' || value === 'X'){
-        return true
-    }
-    return 'Wrong input. Please press "x" followed by "enter" to quit the program'
-}
+  if (value === "x" || value === "X") {
+    return true;
+  }
+  return 'Wrong input. Please press "x" followed by "enter" to quit the program';
+};
